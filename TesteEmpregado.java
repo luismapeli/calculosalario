45f4b32c4c89ea923c40e3e05a6104510af4270f
Luis Henrique Alves Mapeli <luis.alves.mapeli@gmail.com>
2021-03-08 20:34:18 -0300

    Teste Empregados 1

---
+++ TesteEmpregado.java
@@ -0,0 +1,7 @@
+public Class TesteEmpregado {
+    public class void main(String[] args) {
+        Empregado e1 = new Empregado();
+        Empregado e2 = new Empregado();
+        Empregado e3 = new Empregado();
+    }
+}
