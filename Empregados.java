7e6ddee16c50bc490266a737b657fbb6a445884a
Luis Henrique Alves Mapeli <luis.alves.mapeli@gmail.com>
2021-03-08 21:18:47 -0300

    teste

--- Empregados.java
+++ Empregados.java
@@ -1,47 +1,69 @@
 public class Empregado{
-private int tipo;
-private double salario;
-private double comissao;
-private double bonus;
-private String nome;
-private int idade;
-
-
-
-public double calcularSalario(){
-    switch (tipo) {
-        case 1:
-            return salario;
-
-                case 2: 
-                    return salario + salario*comissao;
-        
-                        case 3: 
-                            return salario+bonus;
-     
-                                default:
-                                    return 0;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+    private String nome;
+    private int idade;
+  
+    public int getTipo() {
+      return tipo;
     }
-}
-
-
- public int getTipo(){
-    return tipo;
- }
- public void setTipo {
-    this.tipo = tipo;
- }
-  public String getNome() {
-    return nome;
-  }
-  public int getIdade() {
-    return idade;
-  }
-
-  public void setIdade(int idade) {
-    this.idade = idade;
-  }
-  public void setNome(String nome) {
-    this.nome = nome;
-  }
-}
+    public void setTipo(int tipo) {
+      this.tipo = tipo;
+    }
+  
+  
+    public double calcularSalario(){
+      switch (tipo){
+        case 1:
+          return salario;
+        case 2:
+          return salario + salario * comissao;
+        case 3:
+          return salario + bonus;
+        default:
+          return 0;
+      }
+    }
+  
+    public String getNome() {
+      return nome;
+    }
+    public int getIdade() {
+      return idade;
+    }
+  
+    public void setIdade(int idade) {
+      this.idade = idade;
+    }
+    public void setNome(String nome) {
+      this.nome = nome;
+    }
+  
+    public void setSalario(double salario) {
+      this.salario = salario;
+    }
+  
+    public double getSalario() {
+      return salario;
+    }
+  
+    public void setBonus(double bonus) {
+      this.bonus = bonus;
+    }
+  
+    public double getBonus() {
+      return bonus;
+    }
+  
+    public void setComissao(doublepublic void setComissao(double comissao) {
+        this.comissao = comissao;
+      }
+    
+      public double getComissao() {
+        return comissao;
+      }
+    }
+    
+  
